272dffe3a84e5dae11b3bb95b426f8454bab08df
teodoratrinca <teodora.trinca@gmail.com>
2020-12-16 20:05:23 +0200

    add service layer

--- app/main/controllers/movie_controller.py
+++ app/main/controllers/movie_controller.py
@@ -1,5 +1,7 @@
+from flask import request
 from flask_restx import Resource
 from ..util.dto import MovieDTO
+from ..services.movie_service import get_movies
 
 api = MovieDTO.api
 _movie = MovieDTO.movie
@@ -9,10 +11,25 @@ _movie = MovieDTO.movie
 class Movie(Resource):
 
     # specifies the fields to use for serialization
+    @api.doc(
+        description="""
+        Returns all movies with a given title.
+        """,
+        params={
+            'title': {
+                'description': 'The title of the movie <br>',
+                'type': 'string'
+            }
+        }
+    )
     @api.marshal_list_with(_movie, envelope='movies')
     @api.response(200, 'Success')
     def get(self):
+        title = request.args.get('title')
+
+        get_movies(title)
+
         return [{
-            'title': 'Avengers',
+            'title': title,
             'rating': 9.8
         }]
