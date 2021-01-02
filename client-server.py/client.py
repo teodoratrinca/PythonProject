0d15f402bfd819fa0237f19d6c68fdeede70b86a
teodoratrinca <teodora.trinca@gmail.com>
2021-01-02 20:33:15 +0200

    Add GUI

--- client-server.py/client.py
+++ client-server.py/client.py
@@ -1,22 +1,96 @@
+
 import socket
-import requests
-import sys
-import pickle
+from tkinter import *
+
+
 
 host = '127.0.0.1'
 port = 2727
+#address = (host,port)
+
+format="utf-8"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-while True:
-        msg = s.recv(4096)
-        print(msg.decode("utf-8"))
-        movie = input("Filmul despre care doresc sa aflu mai multe informatii: ")
-        s.sendall(str.encode(movie))
-        msg = s.recv(4096)  # primesc ratingul despre filmul pe care il doresc
-        recd = pickle.loads(msg)
-        print(recd)
-        #print(type(recd))
+
+# GUI class for the INFO
+class GUI:
+        def __init__(self):
+            self.Window = Tk()
+            self.Window.withdraw()
+
+            #search window
+            self.search = Toplevel()
+            self.search.title(" Movie Rating ")
+            self.search.config(width = 700, height = 800)
+
+            #create the labels
+            self.label1 = Label(self.search,text= "Type your Movie Title:",justify = CENTER,font = "Helvetica 14 bold")
+            self.label1.place(relheight=0.16, rely=0.01, relx=0.10)
+
+
+            self.movieName = Entry(self.search,font="Helvetica 14")
+            self.movieName.place(relwidth=0.4,relheight=0.10, rely=0.16, relx=0.10)
+            self.movieName.focus()
+
+            # create a Search Button
+            self.rating = Button(self.search, text="Search for rating", font="Helvetica 14 bold", command=lambda: self.get1(self.movieName.get()))
+            self.rating.place(rely=0.28, relx=0.10)
+
+
+
+            self.label2 = Label(self.search, text="Search by actor:", justify=CENTER,font="Helvetica 14 bold")
+            self.label2.place(relheight=0.16, rely=0.33, relx=0.10)
+            self.actor = Entry(self.search,font="Helvetica 14")
+            self.actor.place(relwidth=0.4,relheight=0.10, rely=0.46, relx=0.10)
+            self.actor.focus()
+
+            # create a Search Button
+            self.rating_actor = Button(self.search,text="Search for rating", font="Helvetica 14 bold",
+                                       command=lambda: self.get2(self.actor.get()))
+            self.rating_actor.place(rely=0.58, relx=0.10)
+
+
+            self.label3=Label(self.search, text="Search by a part of movie title:", justify=CENTER,font="Helvetica 14 bold")
+            self.label3.place(relheight=0.16, rely=0.63, relx=0.10)
+            self.title = Entry(self.search, font="Helvetica 14")
+            self.title.place(relwidth=0.4, relheight=0.10, rely=0.76, relx=0.10)
+            self.title.focus()
+            self.rating_title = Button(self.search, text="Search for rating", font="Helvetica 14 bold",
+                                       command=lambda: self.get3(self.title.get()))
+            self.rating_title.place(rely=0.88, relx=0.10)
+            self.Window.mainloop()
+
+
+        def get1(self,movieName):
+            s.send(('TITLE').encode(format))
+            message = movieName
+            s.send(message.encode(format))
+            self.movieName= s.recv(1024).decode(format)
+            self.label4 = Label(self.search, text=self.movieName, font=('helvetica', 10, 'bold'))
+            self.label4.place( rely=0.16, relx=0.55)
+
+        def get2(self,actor):
+            s.send(('ACTOR').encode(format))
+            message = actor
+            s.send(message.encode(format))
+            self.actor= s.recv(1024).decode(format)
+            self.label4 = Label(self.search, text=self.actor, font=('helvetica', 10, 'bold'))
+            self.label4.place(rely=0.46, relx=0.55)
+
+        def get3(self,word):
+            s.send(('WORD').encode(format))
+            message =word
+            s.send(message.encode(format))
+            self.word= s.recv(1024).decode(format)
+            self.label4 = Label(self.search, text=self.word, font=('helvetica', 10, 'bold'))
+            self.label4.place(rely=0.76, relx=0.55)
+
+
+# create a GUI class object
+g= GUI()
+
+
 
 
 
--- client-server.py/server.py
+++ client-server.py/server.py
@@ -1,155 +1,93 @@
-import pickle
+
+
 import pymongo
 import socket
 from pprint import PrettyPrinter
-import tkinter as tk
+
+pp = PrettyPrinter()
+format = "utf-8"
+
+host = '127.0.0.1'
+port = 2727
+# Address is stored as a tuple
+#address = (host, port)
+
+# Create a new socket for
+# the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["MovieDB"]
 mycol = mydb["movies"]
-root= tk.Tk()
-
-canvas1 = tk.Canvas(root, width = 700, height = 600,  relief = 'raised')
-canvas1.pack()
-
-label1 = tk.Label(root, text='Movies Ratings')
-label1.config(font=('helvetica', 14))
-canvas1.create_window(300, 25, window=label1)
-
-label2 = tk.Label(root, text='Type your Movie Title:')
-label2.config(font=('helvetica', 10))
-canvas1.create_window(300, 100, window=label2)
-
-label3 = tk.Label(root, text='Search by actor:')
-label3.config(font=('helvetica', 10))
-canvas1.create_window(300, 260, window=label3)
-
-label4 = tk.Label(root, text='Search by a part of movie title:')
-label4.config(font=('helvetica', 10))
-canvas1.create_window(300, 400, window=label4)
-
-entry1 = tk.Entry (root)
-canvas1.create_window(300, 140, window=entry1)
-entry2 = tk.Entry (root)
-canvas1.create_window(300, 290, window=entry2)
-entry3 = tk.Entry (root)
-canvas1.create_window(300, 430, window=entry3)
-
-def ratings(title):
-  myquery={'Title':title}
-  mydoc = mycol.find(myquery)
-  for x in mydoc:
-     return (x['imdbRating'])
-  return ("Nu s a gasit filmul")
-#cautare dupa titlu
-
-def cautare_dupa_cuvant(word):
-  gasit = 0
-  allmovie = ""
-  for x in mycol.find():
-      titles = {"title": x['Title'], "imdbRating": x['imdbRating']}
-      words = titles['title'].split(' ')
-      if word in words:
-          print(word)
-          gasit = 1
-          allmovie = allmovie + str(titles['title'])
-          allmovie = allmovie + str(" with rating: ")
-          allmovie = allmovie + str(titles['imdbRating'])
-          allmovie = allmovie + str(" \n")
-  if (gasit == 0):
-      return ("Nu s-a putu gasi film")
-  else:
-      return (allmovie)
 
 
-#print(cautare_dupa_titlu("Titanic"))
+def getRating( name):
+    myquery = {'Title': name}
+    mydoc = mycol.find(myquery)
+    for x in mydoc:
+        return (x['imdbRating'])
+    return ("Nu s a gasit filmul")
+
+def getRatingByActor( actor):
+    gasit = 0
+    allmovie = ""
+    for x in mycol.find():
+        actors = {"title": x['Title'], "actors": x['Actors'], "imdbRating": x['imdbRating']}
+        words = actors['actors'].split(', ')
+        print(words)
+        if actor in words:
+            gasit = 1
+            allmovie = allmovie + str(actors['title'])
+            allmovie = allmovie + str(" with rating: ")
+            allmovie = allmovie + str(actors['imdbRating'])
+            allmovie = allmovie + str(" \n")
+    if (gasit == 0):
+        return ("Nu s-a putu gasi film")
+    else:
+        return (allmovie)
+
+def getRatingByWord( word):
+    gasit = 0
+    allmovie = ""
+    for x in mycol.find():
+        titles = {"title": x['Title'], "imdbRating": x['imdbRating']}
+        words = titles['title'].split(' ')
+        if word in words:
+            print(word)
+            gasit = 1
+            allmovie = allmovie + str(titles['title'])
+            allmovie = allmovie + str(" with rating: ")
+            allmovie = allmovie + str(titles['imdbRating'])
+            allmovie = allmovie + str(" \n")
+    if (gasit == 0):
+        return ("Nu s-a putu gasi film")
+    else:
+        return (allmovie)
+
+def start():
+    print("Server is working on " + host)
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        message = conn.recv(1024).decode(format)
+        if message == 'TITLE':
+            message1 = conn.recv(1024).decode(format)
+            conn.send((getRating(message1)).encode(format))
+        if message =='ACTOR':
+            message1 = conn.recv(1024).decode(format)
+            conn.send((getRatingByActor(message1)).encode(format))
+        if message =='WORD':
+            message1 = conn.recv(1024).decode(format)
+            conn.send((getRatingByWord(message1)).encode(format))
 
 
-def cautare_dupa_actor(actor):
-  gasit=0
-  allmovie=""
-  for x in mycol.find():
-    actors ={ "title": x['Title'],"actors": x['Actors'],"imdbRating": x['imdbRating']}
-    words = actors['actors'].split(', ')
-    print(words)
-    if actor in words:
-        gasit=1
-        allmovie= allmovie+str(actors['title'])
-        allmovie = allmovie + str(" with rating: ")
-        allmovie= allmovie+str( actors['imdbRating'])
-        allmovie= allmovie +str(" \n")
-  if (gasit == 0):
-    return ("Nu s-a putu gasi film")
-  else:
-      return(allmovie)
-
-#print(cautare_dupa_actor('Kate Winslet'))
-
-def getRating():
-    x1 = entry1.get()
-
-    label3 = tk.Label(root, text='The Rating of the movie is:', font=('helvetica', 10))
-    canvas1.create_window(300, 210, window=label3)
-
-    label4 = tk.Label(root, text=ratings(x1), font=('helvetica', 10, 'bold'))
-    canvas1.create_window(300, 230, window=label4)
-def getRatingByActor():
-    x1 = entry2.get()
-
-    label3 = tk.Label(root, text='The rating of movies in which the actor plays:', font=('helvetica', 10))
-    canvas1.create_window(300, 320, window=label3)
-
-    label4 = tk.Label(root, text=cautare_dupa_actor(x1), font=('helvetica', 10, 'bold'))
-    canvas1.create_window(300, 370, window=label4)
-def getRatingByTitle():
-    x1 = entry3.get()
-
-    label3 = tk.Label(root, text='The rating of movies containing the name:', font=('helvetica', 10))
-    canvas1.create_window(300, 470, window=label3)
-
-    label4 = tk.Label(root, text=cautare_dupa_cuvant(x1), font=('helvetica', 10, 'bold'))
-    canvas1.create_window(300, 510, window=label4)
+start()
 
 
-button1 = tk.Button(text='Get the Rating', command=getRating, bg='brown', fg='white',
-                    font=('helvetica', 9, 'bold'))
-canvas1.create_window(300, 180, window=button1)
-button2 = tk.Button(text='Get the Rating', command=getRatingByActor, bg='brown', fg='white',
-                    font=('helvetica', 9, 'bold'))
-canvas1.create_window(450, 290, window=button2)
-button4 = tk.Button(text='Get the Rating', command=getRatingByTitle, bg='brown', fg='white',
-                    font=('helvetica', 9, 'bold'))
-canvas1.create_window(450, 430, window=button4)
-
-root.mainloop()
 
 
-# pp = PrettyPrinter()
-#
-# host = '127.0.0.1'
-# port = 2727
-#
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((host, port))
-# s.listen(10)
-#
 
-
-#
-# while True:
-#     clientsocket, addr = s.accept()
-#     print(f"Conexiunea cu {addr} a fost stabilita cu succes!")
-#     clientsocket.send(bytes("Bine ati venit", "utf-8"))
-#     msg = clientsocket.recv(4096)
-#     title = msg.decode("utf-8")
-#     print("Clientul doreste ratingul filmului: ", title)
-#     data = ratings(title)
-#     msg = pickle.dumps(data)
-#     print(msg)
-#     clientsocket.send(msg)
-#     #print(type(data))
-#     #print(data)
-#
-#
 
 
 
